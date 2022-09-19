10f25d66a883c8749e37cccf003bbfbdd4abb8ce
Siddharth Patel <siddharth.patel@antiersolutions.com>
2022-09-19 17:59:18 +0530

    Initial push

---
+++ test/sample-test.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    let x = await provider.getStorageAt('0x5a6D2401c203E694b2951ac2F987AecCBcFB00db', slot);
+  });
+});
